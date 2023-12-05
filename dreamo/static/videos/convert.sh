2fde9e97f9a616409c2e3f18745a324f7a0a37a1
ttaoREtw <ttaoREtw@gmail.com>
2023-12-05 09:38:44 +0000

    Deploying to gh-pages from @ ttaoREtw/ttaoREtw.github.io@82f92f817751622ec5c8e63a73701b1b5a9ddb5e 🚀

---
+++ dreamo/static/videos/convert.sh
@@ -0,0 +1,17 @@
+
+
+ffmpeg -i raw/cat_bone_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 cat_bone_0.mp4
+ffmpeg -i raw/cat_color_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 cat_color_0.mp4
+
+ffmpeg -i raw/cow_bone_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 cow_bone_0.mp4
+ffmpeg -i raw/cow_color_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 cow_color_0.mp4
+
+ffmpeg -i raw/rabbit_bone_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 rabbit_bone_0.mp4
+ffmpeg -i raw/rabbit_color_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 rabbit_color_0.mp4
+
+ffmpeg -i raw/elephant_bone_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 elephant_bone_0.mp4
+ffmpeg -i raw/elephant_color_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 elephant_color_0.mp4
+
+
+ffmpeg -i raw/polarbear_bone_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 polarbear_bone_0.mp4
+ffmpeg -i raw/polarbear_color_0.mp4 -preset slow -codec:a libfdk_aac -b:a 128k -codec:v libx264 -pix_fmt yuv420p -b:v 4500k -minrate 4500k -maxrate 9000k -bufsize 9000k -vf scale=-1:1080 polarbear_color_0.mp4
